6b61e0e8a02aded2d2a6080eef539f5e34f4521a
SarrankanPathmanathan <sarran_01@hotmail.ca>
2017-02-16 14:13:54 -0500

    Update AddressBookController.java

--- src/main/java/AddressBook/AddressBookController.java
+++ src/main/java/AddressBook/AddressBookController.java
@@ -1,23 +1,37 @@
 package AddressBook;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 @Controller
 public class AddressBookController {
 
-    @GetMapping("/Books")
-    public String addressBookForm(Model model) {
-      model.addAttribute("Books", new AddressBook());
-      return "Books";
+
+    @Autowired
+    BuddyInfoRepository brepository;
+
+    @Autowired
+    AddressBookRepository arepository;
+
+    @GetMapping("/")
+    public String greetingForm(Model model) {
+       model.addAttribute("books", new AddressBook());
+        model.addAttribute("buddy", new BuddyInfo());
+        model.addAttribute("buddies", brepository.findAll());
+        model.addAttribute("addressbooks", arepository.findAll());
+        return "books";
     }
-    @PostMapping("/Books")
-    public String addressBookSubmit(@ModelAttribute AddressBook addressBook) {
+
+    @PostMapping("/")
+    public String addressBookSubmit(@ModelAttribute("buddy") BuddyInfo buddy, @ModelAttribute("book") AddressBook book) {
+        arepository.save(book);
+        brepository.save(buddy);
         return "result";
     }
+
 
 }
